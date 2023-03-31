2b86b98a6709027fdb57b0ad94f0b4a20752561d
Camilo Restrepo <camilorg2017@gmail.com>
2023-03-31 10:46:41 -0500

    update all styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,26 +2,38 @@
   <form @submit.prevent="submitForm">
     <div class="container">
       <div v-if="pasoActual === 1">
-        <h2>Paso 1</h2>
+        <h2 class="font-bold text-3xl mb-4">Paso 1</h2>
 
-        <label for="pais">País</label>
-        <select id="pais" v-model="paisSeleccionado">
+        <label for="pais" class="block font-bold mb-2">País</label>
+        <select
+          id="pais"
+          v-model="paisSeleccionado"
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
+          required
+        >
           <option v-for="pais in paises" :value="pais" :key="pais">
             {{ pais }}
           </option>
         </select>
 
-        <label for="genero">Género</label>
-        <select id="genero" v-model="genero">
+        <label for="genero" class="block font-bold mb-2">Género</label>
+        <select
+          id="genero"
+          v-model="genero"
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
+          required
+        >
           <option v-for="genero in generos" :value="genero" :key="genero">
             {{ genero }}
           </option>
         </select>
 
-        <label for="primer-nombre">Primer Nombre</label>
-        <h3 v-if="errores.length">&times;</h3>
-        <h3 v-else>&#10003;</h3>
+        <label for="primer-nombre" class="block font-bold mb-2"
+          >Primer Nombre</label
+        >
+
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
           type="text"
           id="primer-nombre"
           v-model="primerNombre"
@@ -29,46 +41,81 @@
           required
         />
 
-        <label for="segundo-nombre">Apellido</label>
+        <label for="segundo-nombre" class="block font-bold mb-2"
+          >Segundo Nombre</label
+        >
+
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
           type="text"
-          id="primer-nombre"
+          id="segundo-nombre"
           v-model="segundoNombre"
-          @input="validarCampo('primerNombre', $event.target.value)"
+          @input="validarCampo('segundoNombre', $event.target.value)"
           required
         />
 
-        <label for="fecha-nacimiento">Fecha de Nacimiento</label>
-        <input type="date" id="fecha-nacimiento" v-model="fechaNacimiento" />
+        <label for="fecha-nacimiento" class="block font-bold mb-2"
+          >Fecha de Nacimiento</label
+        >
+        <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
+          type="date"
+          id="fecha-nacimiento"
+          v-model="fechaNacimiento"
+          required
+        />
 
-        <label for="tipo-documento">Tipo de Documento</label>
-        <select id="tipo-documento" v-model="tipoDocumento">
+        <label for="tipo-documento" class="block font-bold mb-2"
+          >Tipo de Documento</label
+        >
+        <select
+          id="tipo-documento"
+          v-model="tipoDocumento"
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
+          required
+        >
           <option value="cedula">Cedula de ciudadanía</option>
           <option value="pasaporte">Pasaporte</option>
           <option value="cedula-extranjeria">Cedula de extranjería</option>
         </select>
 
-        <label for="numero-documento">Número de Documento</label>
-        <input type="number" id="numero-documento" v-model="numeroDocumento" />
-
-        <label for="foto-frente">Foto del Documento - Frente</label>
+        <label for="numero-documento" class="block font-bold mb-2"
+          >Número de Documento</label
+        >
         <input
+          type="number"
+          id="numero-documento"
+          v-model="numeroDocumento"
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
+          required
+        />
+
+        <label for="foto-frente" class="block font-bold mb-2"
+          >Foto del Documento - Frente</label
+        >
+        <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
           type="file"
           id="foto-frente"
           accept=".jpg"
           @change="cargarFotoFrente"
+          required
         />
 
-        <label for="foto-reverso">Foto del Documento - Reverso</label>
+        <label for="foto-reverso" class="block font-bold mb-2"
+          >Foto del Documento - Reverso</label
+        >
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4"
           type="file"
           id="foto-reverso"
           accept=".jpg"
           @change="cargarFotoReverso"
+          required
         />
 
         <button
-          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded font-bold cursor-pointer"
           @click="validarFormularioYAvanzar"
         >
           Siguiente
@@ -76,117 +123,115 @@
       </div>
 
       <div v-if="pasoActual === 2">
-        <h2>Paso 2</h2>
-        <label for="correo-electronico">Correo electrónico</label>
+        <h2 class="font-bold text-3xl mb-4">Paso 2</h2>
+        <label for="correo-electronico" class="block font-bold mb-2"
+          >Correo electrónico</label
+        >
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="email"
-          class="form-control"
           id="correo-electronico"
           v-model="correoElectronico"
           @input="validarCampo('correoElectronico', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('correoElectronico')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
 
-        <label for="contrasena">Contraseña</label>
+        <label for="contrasena" class="block font-bold mb-2">Contraseña</label>
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="password"
-          class="form-control"
           id="contrasena"
           v-model="contrasena"
           @input="validarCampo('contrasena', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('contrasena')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
 
-        <label for="confirmar-contrasena">Confirmar contraseña</label>
+        <label for="confirmar-contrasena" class="block font-bold mb-2"
+          >Confirmar contraseña</label
+        >
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="password"
-          class="form-control"
           id="confirmar-contrasena"
           v-model="confirmarContrasena"
           @input="validarCampo('confirmarContrasena', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('confirmarContrasena')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
 
-        <label for="telefono">Teléfono</label>
+        <label for="telefono" class="block font-bold mb-2">Teléfono</label>
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="tel"
-          class="form-control"
           id="telefono"
           v-model="telefono"
           @input="validarCampo('telefono', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('telefono')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
 
-        <label for="celular">Celular</label>
+        <label for="celular" class="block font-bold mb-2">Celular</label>
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="tel"
-          class="form-control"
           id="celular"
           v-model="celular"
           @input="validarCampo('celular', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('celular')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
 
-        <div v-if="errores.length">
-          <h3>Errores de Validación</h3>
-          <ul>
-            <li v-for="error in errores" :key="error">{{ error }}</li>
-          </ul>
+        <div class="flex justify-between">
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded font-bold cursor-pointer"
+            @click="retrocederPaso"
+          >
+            Retroceder
+          </button>
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded font-bold cursor-pointer"
+            @click="validarFormularioYAvanzar"
+          >
+            Siguiente
+          </button>
         </div>
-        <button @click="retrocederPaso">Retroceder</button>
-        <button @click="validarFormularioYAvanzar">Siguiente</button>
       </div>
       <div v-if="pasoActual === 3">
-        <h2>Paso 3</h2>
-        <label for="direccion">Dirección de residencia</label>
+        <h2 class="font-bold text-3xl mb-4">Paso 3</h2>
+        <label for="direccion" class="block font-bold mb-2"
+          >Dirección de residencia</label
+        >
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="text"
-          class="form-control"
           id="direccion"
           v-model="direccion"
           @input="validarCampo('direccion', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('direccion')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
 
-        <label for="codigo-postal">Código Postal</label>
+        <label for="codigo-postal" class="block font-bold mb-2"
+          >Código Postal</label
+        >
         <input
+          class="w-full py-2 px-4 border border-gray-300 rounded mb-4 form-control"
           type="text"
-          class="form-control"
           id="codigo-postal"
           v-model="codigoPostal"
           @input="validarCampo('codigoPostal', $event.target.value)"
+          required
         />
-        <i
-          v-if="errores.includes('codigoPostal')"
-          class="fa fa-times text-danger"
-        ></i>
-        <i v-else class="fa fa-check text-success"></i>
-        <button @click="retrocederPaso">Retroceder</button>
-        <button @click="enviarFormulario">Enviar</button>
+        <div class="flex justify-between">
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded font-bold cursor-pointer"
+            @click="retrocederPaso"
+          >
+            Retroceder
+          </button>
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded font-bold cursor-pointer"
+            @click="enviarFormulario"
+          >
+            Enviar
+          </button>
+        </div>
       </div>
     </div>
   </form>
@@ -227,12 +272,7 @@ export default {
       generos: ["Masculino", "Femenino", "Otro"],
       genero: "",
       pasoActual: 1,
-      campo1: "",
-      campo2: "",
-      campo3: "",
-      campo1Valido: false,
-      campo2Valido: false,
-      campo3Valido: false,
+
       primerNombre: "",
       segundoNombre: "",
       fechaNacimiento: "",
@@ -242,7 +282,7 @@ export default {
       fotoReverso: null,
       correoElectronico: "",
       contrasena: "",
-      confirmacionContrasena: "",
+      confirmarContrasena: "",
       numeroTelefono: "",
       numeroCelular: "",
       direccionResidencia: "",
@@ -276,23 +316,7 @@ export default {
     retrocederPaso() {
       this.pasoActual -= 1;
     },
-    validarCampo1() {
-      this.campo1Valido = this.campo1.trim() !== "";
-      return this.campo1Valido;
-    },
-    validarCampo2() {
-      this.campo2Valido = this.campo2.trim() !== "";
-      return this.campo2Valido;
-    },
-    validarCampo3() {
-      this.campo3Valido = this.campo3.trim() !== "";
-      return this.campo3Valido;
-    },
-    validarCampos() {
-      return (
-        this.validarCampo1() && this.validarCampo2() && this.validarCampo3()
-      );
-    },
+
     enviarFormulario() {
       console.log("País:", this.paisSeleccionado);
       console.log("Género:", this.genero);
@@ -301,10 +325,13 @@ export default {
       console.log("Fecha de Nacimiento:", this.fechaNacimiento);
       console.log("Tipo de Documento:", this.tipoDocumento);
       console.log("Número de Documento:", this.numeroDocumento);
+
       console.log("Correo Electronico:", this.correoElectronico);
-      this.contrasena = "*********";
+      this.contrasena = "••••••••••";
       console.log("Contraseña:", this.contrasena);
-      console.log("Dirección Residencia:", this.direccionResidencia);
+      console.log("Telefono:", this.telefono);
+      console.log("Celular:", this.celular);
+      console.log("Dirección Residencia:", this.direccion);
       console.log("Código Postal:", this.codigoPostal);
 
       // mostrar el modal de confirmación
@@ -335,7 +362,18 @@ export default {
       let telefonoRegex;
       let celularRegex;
       let codigoPostalRegex;
+      let numeroDocumentoRegex = /^\d{5,}$/;
       switch (campo) {
+        case "numeroDocumento":
+          if (!numeroDocumentoRegex.test(valor)) {
+            this.errores.push("El número de documento no es válido");
+          } else {
+            this.errores = this.errores.filter(
+              (error) => error !== "El número de documento no es válido"
+            );
+          }
+          break;
+
         case "primerNombre":
           if (valor.trim() === "") {
             this.errores.push("El primer nombre es requerido");
@@ -432,48 +470,6 @@ export default {
           break;
       }
     },
-
-    validarFormulario() {
-      this.errores = [];
-
-      if (!this.primerNombre) {
-        this.errores.push("El nombre esta vacio");
-      }
-
-      if (!this.correoElectronicoValido) {
-        this.errores.push("El correo electrónico no es válido");
-      }
-
-      if (!this.contrasenaValida) {
-        this.errores.push("La contraseña debe tener al menos 8 caracteres");
-      }
-
-      if (!this.confirmacionContrasenaValida) {
-        this.errores.push("La confirmación de la contraseña no coincide");
-      }
-
-      if (!this.telefonoValido && !this.celularValido) {
-        this.errores.push(
-          "Se requiere al menos un número de teléfono o celular"
-        );
-      }
-
-      if (!this.paisSeleccionadoValido) {
-        this.errores.push("Debe seleccionar un país");
-      }
-
-      if (this.errores.length === 0) {
-        this.$emit("avanzar-paso");
-      }
-    },
-
-    validarPais() {
-      if (this.paisSeleccionado === "") {
-        this.errores.push("Debe seleccionar un país");
-        return false;
-      }
-      return true;
-    },
   },
 
   created() {
@@ -481,26 +477,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.icono-validacion {
-  font-size: 1rem;
-  margin-left: 0.5rem;
-}
-
-.icono-validacion::before {
-  display: inline-block;
-  font-family: "Font Awesome 5 Free";
-  font-weight: 900;
-}
-
-.icono-validacion::before {
-  content: "\f00c";
-  color: green;
-}
-
-.icono-validacion:not([v-if])::before {
-  content: "\f00d";
-  color: red;
-}
-</style>
